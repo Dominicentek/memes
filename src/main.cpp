c1834f9e66aed4fce823c84cf1a1f89829d63fe0
Dominicentek <dominicentekgaming@gmail.com>
2023-12-17 16:04:44 +0100

    search memes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <cstring>
+#include <algorithm>
 
 #include "ansi.h"
 #include "settings.h"
@@ -9,6 +11,7 @@
 
 #define MAX_MEME_LIMIT 16
 #define BEGINNING_ITEMS 5
+#define MEME_SEARCH_MAX 256
 #define FLUSH fflush(nullptr)
 #define END return end()
 
@@ -17,6 +20,8 @@ int optionpos[] = { 3, 4, 5, 6, 8, 11 };
 std::filesystem::path meme_folder = std::filesystem::path(MEME_FOLDER_SRC);
 std::vector<std::filesystem::path> all_memes = {};
 int meme_width = 0;
+int meme_search_len = 0;
+char meme_search[MEME_SEARCH_MAX];
 
 int selected_item = 0;
 int selected_meme = 0;
@@ -24,18 +29,28 @@ int selected_meme_opt = 0;
 
 void refresh_directory() {
     all_memes.clear();
+    std::string search_term = std::string(meme_search);
+    std::transform(search_term.begin(), search_term.end(), search_term.begin(), ::tolower);
     for (auto& entry : std::filesystem::recursive_directory_iterator(meme_folder)) {
         if (std::filesystem::is_directory(entry.path())) continue;
         if (meme_width < entry.path().filename().string().length()) meme_width = entry.path().filename().string().length();
+        std::string filename = entry.path().filename();
+        std::transform(filename.begin(), filename.end(), filename.begin(), ::tolower);
+        if (filename.find(search_term) == std::string::npos) continue;
         all_memes.push_back(entry.path());
     }
 }
 
 void print_meme_table() {
     int offset = selected_item - BEGINNING_ITEMS - MAX_MEME_LIMIT / 2;
-    offset = std::min(std::max(offset, 0), (int)all_memes.size() - MAX_MEME_LIMIT);
+    offset = std::min(std::max(offset, 0), std::max((int)all_memes.size() - MAX_MEME_LIMIT, 0));
+    printf(MOVE_ABSOLUTE_FMT BG_COLOR_DEFAULT FG_COLOR_DEFAULT CLEAR_LINE FG_COLOR_YELLOW "%d" FG_COLOR_DEFAULT " memes found", optionpos[BEGINNING_ITEMS] - 1, 7, all_memes.size());
     for (int i = 0; i < MAX_MEME_LIMIT; i++) {
-        std::string filename = all_memes[offset + i].filename().string().c_str();
+        if (i >= all_memes.size()) {
+            printf(MOVE_ABSOLUTE_FMT BG_COLOR_DEFAULT FG_COLOR_DEFAULT CLEAR_LINE, optionpos[BEGINNING_ITEMS] + i, 7);
+            continue;
+        }
+        std::string filename = all_memes[offset + i].filename().string();
         printf(MOVE_ABSOLUTE_FMT BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ %s%s ] ", optionpos[BEGINNING_ITEMS] + i, 7, filename.c_str(), std::string(meme_width - filename.length(), ' ').c_str());
     }
 }
@@ -68,6 +83,11 @@ int end() {
     return 0;
 }
 
+void print_search() {
+    int margin = std::max(0, 16 - meme_search_len);
+    printf(MOVE_ABSOLUTE_FMT BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ 🔎 %s%s ] ", optionpos[4], 7, meme_search + std::max(0, meme_search_len - 16), std::string(margin, '_').c_str());
+}
+
 int main() {
     refresh_directory();
     printf(CLEAR_SCREEN);
@@ -75,11 +95,10 @@ int main() {
     printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Send Random ] ",         optionpos[1], 7);
     printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Download    ] ",         optionpos[2], 7);
     printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Refresh     ] ",         optionpos[3], 7);
-    printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ 🔎 ________________ ] ", optionpos[4], 7);
+    print_search();
     printf(MOVE_ABSOLUTE(3, 37) BG_COLOR_DEFAULT FG_COLOR_DEFAULT "Use the arrow keys to navigate.");
     printf(MOVE_ABSOLUTE(4, 37) BG_COLOR_DEFAULT FG_COLOR_DEFAULT "Enter to select.");
     printf(MOVE_ABSOLUTE(5, 37) BG_COLOR_DEFAULT FG_COLOR_DEFAULT "Ctrl+C or ESC to quit.");
-    printf(MOVE_ABSOLUTE_FMT FG_COLOR_YELLOW "%d" FG_COLOR_DEFAULT " memes found", optionpos[BEGINNING_ITEMS] - 1, 7, all_memes.size());
     print_meme_table();
     printf(HIDE_CURSOR);
     FLUSH;
@@ -95,7 +114,6 @@ int main() {
             if (selected_item < 0) selected_item = 0;
             if (selected_item > BEGINNING_ITEMS + all_memes.size() - 1) selected_item = all_memes.size() - 1 + BEGINNING_ITEMS;
             print_cursor('>');
-            print_meme_table();
             if (kc == 10) {
                 if (selected_item == 3) refresh_directory();
                 if (selected_item >= BEGINNING_ITEMS) {
@@ -122,6 +140,14 @@ int main() {
                     print_meme_action(true);
                 }
             }
+            if (selected_item == 4 && (kc >= 32 && kc <= 127)) {
+                if (kc == 127) { if (meme_search_len > 0) meme_search_len--; }
+                else if (meme_search_len != MEME_SEARCH_MAX - 1) meme_search[meme_search_len++] = kc;
+                meme_search[meme_search_len] = 0;
+                print_search();
+                refresh_directory();
+            }
+            print_meme_table();
             FLUSH;
         }
     }
