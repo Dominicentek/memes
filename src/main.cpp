d96165fcb13692e7b231b40172cc388796af63d6
Dominicentek <dominicentekgaming@gmail.com>
2024-04-09 21:09:17 +0200

    channel switcher

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,23 @@
 #include <fstream>
 
 #include "ansi.h"
-#include "settings.h"
 #include "kbhit.h"
 
+#define CHANNEL(name, id) { name, id },
+
+std::vector<std::pair<std::string, std::string>> channels = {
+#include "settings.h"
+};
+
 #define MAX_MEME_LIMIT 16
-#define BEGINNING_ITEMS 5
+#define MAX_CHANNEL_LIMIT 5
+#define BEGINNING_ITEMS 6
 #define MEME_SEARCH_MAX 256
 #define FLUSH fflush(nullptr)
 #define END return end()
+#define SLEEP(millis) std::this_thread::sleep_for(std::chrono::milliseconds(millis))
 
-int optionpos[] = { 3, 4, 5, 6, 8, 11 };
+int optionpos[] = { 3, 4, 5, 6, 7, 9, 12 };
 
 std::filesystem::path meme_folder = std::filesystem::path(MEME_FOLDER_SRC);
 std::vector<std::filesystem::path> all_memes = {};
@@ -36,6 +43,10 @@ bool meme_playing = false;
 bool meme_stop_playing = false;
 int meme_counter = 0;
 
+std::string curr_channel_name = channels[0].first;
+std::string curr_channel_id = channels[0].second;
+int curr_channel_index = 0;
+
 void play_meme(int id, int y, int x) {
     meme_playing = true;
     std::thread playing_thread = std::thread([&]() {
@@ -44,7 +55,7 @@ void play_meme(int id, int y, int x) {
         while (meme_playing) {
             printf(MOVE_ABSOLUTE_FMT FG_COLOR_YELLOW BG_COLOR_DEFAULT "%c" FG_COLOR_DEFAULT " Playing", y, x, spin[spinptr++]);
             FLUSH;
-            std::this_thread::sleep_for(std::chrono::milliseconds(125));
+            SLEEP(125);
             if (spinptr == spin.length()) spinptr = 0;
         }
     });
@@ -66,7 +77,7 @@ void play_all_memes() {
             if (meme_stop_playing) printf(MOVE_ABSOLUTE_FMT FG_COLOR_YELLOW BG_COLOR_DEFAULT "%c" FG_COLOR_DEFAULT " Waiting for video to finish", optionpos[0], 25, spin[spinptr++]);
             else printf(MOVE_ABSOLUTE_FMT FG_COLOR_YELLOW BG_COLOR_DEFAULT "%c" FG_COLOR_DEFAULT " (%d/%d)", optionpos[0], 25, spin[spinptr++], meme_counter + 1, all_memes.size());
             FLUSH;
-            std::this_thread::sleep_for(std::chrono::milliseconds(125));
+            SLEEP(125);
             if (spinptr == spin.length()) spinptr = 0;
             while (kb.kbhit()) {
                 if (kb.getch() == 3) meme_stop_playing = true;
@@ -132,10 +143,10 @@ void send_meme(int id, int y, int x) {
             printf(MOVE_ABSOLUTE_FMT FG_COLOR_YELLOW BG_COLOR_DEFAULT "%c" FG_COLOR_DEFAULT " Sending", y, x, spin[spinptr++]);
             if (spinptr == spin.length()) spinptr = 0;
             FLUSH;
-            std::this_thread::sleep_for(std::chrono::milliseconds(125));
+            SLEEP(125);
         }
     });
-    system("curl -X POST --header \"Content-Type: multipart/form-data; boundary=boundary\" --header \"Accept: */*\" --header \"Authorization: " DISCORD_TOKEN "\" --data-binary @/tmp/body.dat https://discord.com/api/v9/channels/" DISCORD_CHANNEL "/messages 1> /dev/null 2> /dev/null");
+    system(("curl -X POST --header \"Content-Type: multipart/form-data; boundary=boundary\" --header \"Accept: */*\" --header \"Authorization: " DISCORD_TOKEN "\" --data-binary @/tmp/body.dat https://discord.com/api/v9/channels/" + curr_channel_id + "/messages 1> /dev/null 2> /dev/null").c_str());
     sending = false;
     sending_thread.join();
     printf(MOVE_ABSOLUTE_FMT "         ", y, x);
@@ -144,6 +155,67 @@ void send_meme(int id, int y, int x) {
 
 int get_random() {
     return rand() % all_memes.size();
+}
+
+void print_channel_switcher() {
+    int offset = curr_channel_index - MAX_CHANNEL_LIMIT / 2;
+    offset = std::min(std::max(offset, 0), std::max((int)channels.size() - MAX_CHANNEL_LIMIT, 0));
+    int maxlen = 0;
+    for (int i = 0; i < channels.size(); i++) {
+        if (maxlen < channels[i].first.length()) maxlen = channels[i].first.length();
+    }
+    for (int i = 0; i <= MAX_CHANNEL_LIMIT; i++) {
+        if (i >= channels.size()) break;
+        if (i == curr_channel_index) printf(MOVE_ABSOLUTE_FMT
+            BG_COLOR_DEFAULT FG_COLOR_DEFAULT
+            BG_COLOR_WHITE FG_COLOR_DARK_BLUE
+            " [ %-*s ] ",
+            optionpos[3] + i, 34, maxlen, channels[i].first.c_str()
+        );
+        else printf(MOVE_ABSOLUTE_FMT
+            BG_COLOR_DEFAULT FG_COLOR_DEFAULT
+            BG_COLOR_DARK_BLUE FG_COLOR_WHITE
+            " [ %-*s ] ",
+            optionpos[3] + i, 34, maxlen, channels[i].first.c_str()
+        );
+    }
+    FLUSH;
+}
+
+void clear_channel_switcher() {
+    printf(BG_COLOR_DEFAULT FG_COLOR_DEFAULT);
+    for (int i = 0; i < 5; i++) {
+        printf(MOVE_ABSOLUTE_FMT CLEAR_CURSOR_TO_LINE_END, optionpos[3] + i, 34);
+    }
+    FLUSH;
+}
+
+void switch_channel() {
+    print_channel_switcher();
+    keyboard kb;
+    int prev = curr_channel_index;
+    while (true) {
+        if (!kb.kbhit()) {
+            SLEEP(50);
+            continue;
+        }
+        int kc = kb.getch();
+        while (kb.kbhit()) kc = kc * 256 + kb.getch();
+        if (kc == 1792833) curr_channel_index--;
+        if (kc == 1792834) curr_channel_index++;
+        if (kc == 3) {
+            curr_channel_index = prev;
+            break;
+        }
+        if (kc == 10) break;
+        if (curr_channel_index < 0) curr_channel_index = 0;
+        if (curr_channel_index >= channels.size()) curr_channel_index = channels.size() - 1;
+        print_channel_switcher();
+        SLEEP(50);
+    }
+    curr_channel_name = channels[curr_channel_index].first;
+    curr_channel_id = channels[curr_channel_index].second;
+    clear_channel_switcher();
 }
 
 void refresh_directory() {
@@ -204,7 +276,7 @@ int end() {
 
 void print_search() {
     int margin = std::max(0, 16 - meme_search_len);
-    printf(MOVE_ABSOLUTE_FMT BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ 🔎 %s%s ] ", optionpos[4], 7, meme_search + std::max(0, meme_search_len - 16), std::string(margin, ' ').c_str());
+    printf(MOVE_ABSOLUTE_FMT BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ 🔎 %s%s ] ", optionpos[5], 7, meme_search + std::max(0, meme_search_len - 16), std::string(margin, ' ').c_str());
 }
 
 int main() {
@@ -214,7 +286,8 @@ int main() {
     printf(MOVE_ABSOLUTE_FMT "> " BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Play All    ] ",         optionpos[0], 5);
     printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Send Random ] ",         optionpos[1], 7);
     printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Play Random ] ",         optionpos[2], 7);
-    printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Refresh     ] ",         optionpos[3], 7);
+    printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Channel     ] ",         optionpos[3], 7);
+    printf(MOVE_ABSOLUTE_FMT      BG_COLOR_DARK_BLUE FG_COLOR_WHITE " [ Refresh     ] ",         optionpos[4], 7);
     print_search();
     print_meme_table();
     printf(HIDE_CURSOR);
@@ -235,14 +308,18 @@ int main() {
                 if (selected_item == 0) play_all_memes();
                 if (selected_item == 1) send_meme(get_random(), optionpos[1], 25);
                 if (selected_item == 2) play_meme(get_random(), optionpos[2], 25);
-                if (selected_item == 3) refresh_directory();
+                if (selected_item == 3) switch_channel();
+                if (selected_item == 4) refresh_directory();
                 if (selected_item >= BEGINNING_ITEMS) {
                     selected_meme = selected_item - BEGINNING_ITEMS;
                     selected_meme_opt = 2;
                     print_meme_action(false);
                     FLUSH;
                     while (true) {
-                        if (!kb.kbhit()) continue;
+                        if (!kb.kbhit()) {
+                            SLEEP(50);
+                            continue;
+                        }
                         kc = kb.getch();
                         while (kb.kbhit()) kc = kc * 256 + kb.getch();
                         if (kc == 3) END;
@@ -258,12 +335,12 @@ int main() {
                         }
                         print_meme_action(false);
                         FLUSH;
-                        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                        SLEEP(50);
                     }
                     print_meme_action(true);
                 }
             }
-            if (selected_item == 4 && (kc >= 32 && kc <= 127)) {
+            if (selected_item == 5 && (kc >= 32 && kc <= 127)) {
                 if (kc == 127) { if (meme_search_len > 0) meme_search_len--; }
                 else if (meme_search_len != MEME_SEARCH_MAX - 1) meme_search[meme_search_len++] = kc;
                 meme_search[meme_search_len] = 0;
@@ -271,11 +348,13 @@ int main() {
                 refresh_directory();
             }
             print_meme_table();
-            if (selected_item == 4) printf(MOVE_ABSOLUTE_FMT SHOW_CURSOR, optionpos[4], 13 + std::min(16, meme_search_len));
+            if (selected_item == 3) printf(MOVE_ABSOLUTE_FMT BG_COLOR_DEFAULT FG_COLOR_DEFAULT "%s", optionpos[3], 25, curr_channel_name.c_str());
+            else printf(MOVE_ABSOLUTE_FMT BG_COLOR_DEFAULT FG_COLOR_DEFAULT CLEAR_CURSOR_TO_LINE_END, optionpos[3], 25);
+            if (selected_item == 5) printf(MOVE_ABSOLUTE_FMT SHOW_CURSOR, optionpos[5], 13 + std::min(16, meme_search_len));
             else printf(HIDE_CURSOR);
             FLUSH;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        SLEEP(50);
     }
     END;
 }
